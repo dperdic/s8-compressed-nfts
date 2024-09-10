3c59311df7d40ee1a2cac4d80f7ca2dc967b2dd0
Dujo Perdic <dujo.perdic1@gmail.com>
2024-09-10 17:38:13 +0200

    types

---
+++ src/utils/types.ts
@@ -0,0 +1,27 @@
+export type NftMetadata = {
+  name: string | undefined;
+  description?: string;
+  image: string;
+  animation_url?: string;
+  external_url?: string;
+  attributes?: NftAttribute[];
+  properties?: Properties;
+};
+
+export type NftAttribute = {
+  trait_type: string;
+  value: string;
+};
+
+export type Properties = {
+  files: NftFile[];
+  category: Category;
+};
+
+export type NftFile = {
+  uri: string;
+  type: string;
+  cdn?: boolean;
+};
+
+export type Category = "video" | "image";
