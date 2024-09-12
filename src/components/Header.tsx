80f88db934bd2c755f5064b9273299cab01294aa
Dujo Perdic <dujo.perdic1@gmail.com>
2024-09-12 02:00:32 +0200

    border

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
 export default function Header() {
   return (
-    <header className="h-18 fixed top-0 z-10 flex w-full bg-white shadow-sm">
+    <header className="fixed top-0 z-10 flex h-18 w-full border-b bg-white shadow-sm">
       <nav className="flex h-full w-full items-center justify-between gap-4 px-8 sm:px-16">
         <img src="/vite.svg" className="h-10" />
 
